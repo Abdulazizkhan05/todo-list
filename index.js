940fb3518b8219e94599a8827d0447775174c291
Abdulazizkhan05 <92906656+Abdulazizkhan05@users.noreply.github.com>
2021-10-23 11:08:16 +0500

    I added another functions

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const todoInput = document.querySelector(".todo-input");
 const btn = document.querySelector(".btn");
 const container = document.querySelector(".container");
 
-const arr = [];
+let i = 0;
+
 btn.addEventListener("click", () => {
-  const input = todoInput.value;
-  if (input.includes("must")) {
-    arr.unshift(`<li>${input}</li>`);
-  } else {
-    arr.push(`<li>${input}</li>`);
-  }
-  container.innerHTML = arr.join("");
-  console.log(input);
+  let div = document.createElement("div");
+  let btnn = document.createElement("button");
+  btnn.textContent = "X";
+  div.textContent = todoInput.value + i++;
+  div.append(btnn);
+  container.append(div);
+  btnn.addEventListener("click", () => {
+    div.remove();
+  });
 });
